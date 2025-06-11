ee76d7d1f2c4842a646a3a91323f66889ee9b13f
Gio Jacuzzi <giojacuzzi@gmail.com>
2025-06-11 13:30:26 -0700

    refactor fig_train_sample_size_perf.R

--- src/figures/fig_train_sample_size_perf.R
+++ src/figures/fig_train_sample_size_perf.R
@@ -2,15 +2,21 @@
 # Calculate and plot results of training sample size experiment
 #
 # Input:
-# - Segment level performance metrics for target models in the sample size experiment (e.g. "results/sample_size_experiment/test/segment_perf/metrics_{model_tag}.csv")
-# -  Sample size counts for target models in the sample size experiment (e.g. "data/models/target/...trained_class_counts.csv")
+# - Segment level performance metrics for target models in the sample size experiment (e.g. "results/{model}/test/segment_perf/metrics_target.csv")
+# - Sample size counts for target models in the sample size experiment (e.g. "data/cache/{model_root}/training/{model}/trained_class_counts.csv")
 #
 # Output:
 # - Plots for training sample size performance experiment (Fig A.3)
 #
 # User-defined parameters
-path_root_all_perf_metrics = 'results/sample_size_experiment'
-path_root_trained_class_counts = 'results/sample_size_experiment' # note that train_fewshot.py generates these to the 'data/models' directory
+models = c(
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N1_I1',
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N5_I1',
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N10_I1',
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N50_I1',
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N100_I1',
+  'target_S1_LR0.001_BS10_HU0_LSFalse_US0_N125_I0' # NOTE: This model is the final OESF_1.0 model
+)
 ##########################################################################################
 
 library(dplyr)
@@ -18,20 +24,23 @@ library(ggplot2)
 library(viridis)
 library(stringr)
 
+model_paths = data.frame(
+  metrics      = paste0('results/', models, '/test/segment_perf/metrics_target.csv'),
+  class_counts = paste0('data/cache/target_S1_LR0.001_BS10_HU0_LSFalse_US0/training/', models, '/trained_class_counts.csv')
+)
+
 # Retrieve all segment level performance metrics for target models in the sample size experiment
-files_metrics = list.files(path = path_root_all_perf_metrics, pattern = "\\metrics_target.csv$", full.names = TRUE, recursive = TRUE)
 metrics = data.frame()
-for (file in files_metrics) {
+for (file in model_paths$metrics) {
   data = read.csv(file)
   data$label = tolower(data$label)
+  data$model_dir = strsplit(file, '/')[[1]][2]
   metrics = rbind(metrics, data)
 }
-metrics$model_dir = gsub("data/test/|/target", "", metrics$model)
 
 # Count the number of training samples for each class
-files_class_counts = unique(file.path(path_root_trained_class_counts, paste0(metrics$model_dir, '/trained_class_counts.csv')))
 class_counts = data.frame()
-for (file in files_class_counts) {
+for (file in model_paths$class_counts) {
   data = read.csv(file)
   names(data)[names(data) == "labels"] <- "label"
   data$label = tolower(sub(".*_", "", data$label))
@@ -49,24 +58,26 @@ origin_points$count = 0
 data = bind_rows(data, origin_points)
 
 # Inspect performance for individual classes
-if (FALSE) {
-  for (class_label in unique(data$label)) {
-    data_l = data[data$label == class_label,]
-    print(data_l[, c('label', 'PR_AUC', 'count')] %>% arrange(count))
-    p = ggplot() +
-      geom_line(data = data_l, aes(x = count, y = PR_AUC)) +
-      geom_point(data = data_l, aes(x = count, y = PR_AUC)) +
-      labs(x = "Training samples", y = "PR AUC", title = class_label) +
-      theme_minimal(); print(p)
-    readline()
-  }
+class_labels = c('pileated woodpecker', 'pacific wren', 'sooty grouse')
+for (class_label in class_labels) {
+  data_l = data[data$label == class_label,]
+  print(data_l[, c('label', 'PR_AUC', 'count')] %>% arrange(count))
+  p = ggplot() +
+    geom_line(data = data_l, aes(x = count, y = PR_AUC)) +
+    scale_x_continuous(breaks = seq(0, 150, by = 10)) +
+    scale_y_continuous(breaks = seq(0, 1.0, by = 0.1)) +
+    geom_point(data = data_l, aes(x = count, y = PR_AUC)) +
+    labs(x = "Training samples", y = "PR AUC", title = class_label) +
+    theme_minimal(); print(p)
 }
 
 # Figure A.3: Plot relationships between training sample size and precision-recall AUC performance across all species classes
 plot_sample_size = ggplot(data = data) +
-  geom_line(aes(x = count, y = PR_AUC, color = count)) +
-  scale_colour_viridis(option = "D") +
-  facet_wrap(~ str_trunc(str_to_title(label), width=24), ncol = 7, scales = "free_x") +
+  geom_line(aes(x = count, y = PR_AUC, color = count), linewidth = 0.75, lineend = 'round') +
+  scale_colour_viridis_c(option = "D", limits = c(0,150), breaks = c(0, 25, 50, 75, 100, 125, 150)) +
+  facet_wrap(~ str_trunc(str_to_title(label), width=24), ncol = 6) +
+  scale_x_continuous(breaks = seq(0, 125, by = 25)) +  # Only set breaks, no limits
+  coord_cartesian(xlim = c(0, 130)) +  # Zoom without dropping data
   labs(color = "Samples", x = "Training sample size", y = "PR AUC") +
   theme_bw() + theme(aspect.ratio = 1, strip.text = element_text(size = 6))
 plot_sample_size
